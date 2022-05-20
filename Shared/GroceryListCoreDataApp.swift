62e6e7e97bd044f66fe32cea79e5f1d6875e4a65
Andy Stefani <andystefani@icloud.com>
2022-05-20 11:45:25 -0700

    Initial Commit

---
+++ Shared/GroceryListCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryListCoreDataApp.swift
+//  Shared
+//
+//  Created by Andy Stefani on 5/20/22.
+//
+
+import SwiftUI
+
+@main
+struct GroceryListCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
